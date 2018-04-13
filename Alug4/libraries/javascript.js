7991afd9827c0b9fc5aad5cdb360be163c91cf0c
Lucas Aragão <lucas.magno@dce.ufpb.br>
2018-04-13 12:28:38 -0300

    frontInitalLogoPackProjeto

---
+++ Alug4/libraries/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	//var i = $('.ind-botao');
+
+	$('.ind-botao').on('click', function(){
+		$('nav li').toggle(); //interrupetor 1 click ou 2 cliks
+	});
+});
+
---
+++ Alug4/views/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Alug</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Empresa especializada em Anuncios de residencias"> <!-- lembrar de mehorar --> 
+			<meta name="keywords" content="Aluguel de casaa, Alug, Aluguel, Alugue, Rio Tinto">
+			<meta name="robots" content="index, fallow">
+			<meta name="author" content="Lucas Aragão">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" type="text/css" href="../libraries/estilo.css">
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans|Pacifico|Roboto+Slab" rel="stylesheet">
+			<script src="../libraries/javascript.js"></script>
+			<script src="../libraries/jquery-3.3.1.min"></script> 
+	</head>
+	
+
+
+	<body>
+
+		<header>
+			<nav>
+				<ul>
+					 <li><a href="index">Home</a></li>
+					 <li><a href="sobrenos">Sobre nós</a></li>
+					 <li><a href="anuncios">Anúncios</a></li>
+					 <li><a href="cadastese">Cadastre-se</a></li>
+					 <li><a href="contato">Contato</a></li>
+				</ul>
+			</nav>	
+
+			<div class="ind-botao"> 
+				<img src="../imagens/botao.png" alt="Botão mobile" title="Botão mobile">
+			</div>
+			
+			<div class="ind-logo"> <!-- criar logo menor para smart -->
+				<picture> <!-- conceito de responsividade pagina 255 --> 
+					<source media = "(max width 480px)" srcset="imagens/logo.png">
+					<source media = "(min width 481px) and (max width 768px)" srcset="imagens/logo.png">
+					<source media = "(min width 769px)" srcset="imagens/logo.png">
+					<img src="../imagens/logo.png ?" alt="Logo Marca da Empresa" title="Logo Alug">  <!-- para navegadores android que n reconhcem picture --> 	
+				</picture>
+			</div>
+
+
+		</header>
+
+		<main>
+			
+		</main>
+
+		<footer>
+			
+		</footer>		
+	
+
+	</body>
+
+
+
+</html>
